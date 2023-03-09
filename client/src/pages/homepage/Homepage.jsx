f35e74c89f76b7124989125c7d195aea96ad17cc
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-03-09 20:15:31 +0000

    client - index.scss updated

--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../../components/header/Header';
 import './Homepage.scss';
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <Header/>
   )
 }
 
